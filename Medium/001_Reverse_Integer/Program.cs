c5666635fb3762eff9f185eb3b18df1f825f4a6f
oneananda <anandathirumurugan@gmail.com>
2024-09-05 20:07:03 +0530

    Adding logic to Reverse_Integer

--- Medium/001_Reverse_Integer/Program.cs
+++ Medium/001_Reverse_Integer/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // https://leetcode.com/problems/reverse-integer/description/
+
+            Console.WriteLine("Enter the integer to reverse!");
+
+            Console.WriteLine($"Output: " + Reverse(Convert.ToInt32(Console.ReadLine())));
+
+            Console.ReadLine();
+        }
+        public static int Reverse(int x)
+        {
+            int result = 0;
+
+            if (x < 0) x = -x;
+
+            string currentValue = x.ToString();
+
+            string convertedValue = string.Empty;
+
+            for (int i = currentValue.Length - 1; i >= 0; i--)
+            {
+                convertedValue += currentValue[i].ToString();
+            }
+
+            result = Convert.ToInt32(convertedValue);
+
+            if (x < 0)
+            {
+                result = -result;
+            }
+
+            return result;
         }
     }
 }
