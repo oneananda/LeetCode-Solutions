f63be8fe5dda1f31e9006895dd14c4eaa447761b
oneananda <anandathirumurugan@gmail.com>
2024-09-06 20:32:31 +0530

    Draft solution

--- Medium/002_String_to_Integer_atoi/Program.cs
+++ Medium/002_String_to_Integer_atoi/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //  https://leetcode.com/problems/string-to-integer-atoi/
             Console.WriteLine("Enter the integer to Convert!");
 
             Console.WriteLine($"Output: " + MyAtoi(Convert.ToString(Console.ReadLine())));
@@ -12,17 +13,47 @@
 
         public static int MyAtoi(string s)
         {
-            var strArr = s.ToArray();
+            var strArr = s.TrimStart().ToArray();
             string convertedValue = string.Empty;
+            bool isNegative = false;
             foreach (var i in strArr)
             {
-                if(Char.IsDigit(i)) 
-                    convertedValue += Convert.ToString(i);
-                else break;
+                if (i == '-')
+                {
+                    isNegative = true;
+                }
+                else
+                {
+                    if (Char.IsDigit(i))
+                        convertedValue += Convert.ToString(i);
+                    else break;
+                }                
+            }
+
+            if (convertedValue == string.Empty)
+            {                
+                convertedValue = "0";
+                isNegative = false;
+            }
+
+            int result = 0;
+            long partialResult = Convert.ToInt64(convertedValue);
+
+            if (partialResult > int.MaxValue)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = Convert.ToInt32(convertedValue);
+                if (isNegative)
+                {
+                    result = -result;
+                }
             }
 
             Console.WriteLine(convertedValue);
-            return s.Length;
+            return result;
         }
     }
 }
