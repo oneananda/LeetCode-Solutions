f70e7c983fd0939ee41b09adcb6234b6cd58ece5
oneananda <anandathirumurugan@gmail.com>
2024-09-07 00:42:14 +0530

    Completed logic for Palindrome_Number

--- Easy/001_Palindrome_Number/Program.cs
+++ Easy/001_Palindrome_Number/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Enter the number to check!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPalindrome(number));
+            Console.ReadLine(); // Hold the screen
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            //char[] strX = x.ToString().ToCharArray();
+            //char[] str2X = strX.Reverse().ToArray();
+            return x.ToString().ToCharArray().SequenceEqual(x.ToString().ToCharArray().Reverse().ToArray());
         }
     }
 }
