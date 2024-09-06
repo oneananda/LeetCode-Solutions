063045168cd0b21749960283578518b641b50fcf
oneananda <anandathirumurugan@gmail.com>
2024-09-06 05:54:26 +0530

    Initial draft

--- Medium/002_String_to_Integer_atoi/Program.cs
+++ Medium/002_String_to_Integer_atoi/Program.cs
@@ -4,11 +4,24 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the integer to Convert!");
+
+            Console.WriteLine($"Output: " + MyAtoi(Convert.ToString(Console.ReadLine())));
             Console.ReadLine(); // Hold the screen
         }
 
         public static int MyAtoi(string s)
         {
+            var strArr = s.ToArray();
+            string convertedValue = string.Empty;
+            foreach (var i in strArr)
+            {
+                if(Char.IsDigit(i)) 
+                    convertedValue += Convert.ToString(i);
+                else break;
+            }
+
+            Console.WriteLine(convertedValue);
             return s.Length;
         }
     }
