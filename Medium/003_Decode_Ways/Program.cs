616d88aff8868cbcffb80aa37f648ca8a742fa66
oneananda <anandathirumurugan@gmail.com>
2024-09-07 20:31:49 +0530

    Partially working

--- Medium/003_Decode_Ways/Program.cs
+++ Medium/003_Decode_Ways/Program.cs
@@ -5,13 +5,56 @@
         static void Main(string[] args)
         {
             // https://leetcode.com/problems/decode-ways/ 
-
+            Console.WriteLine(NumDecodings(Console.ReadLine()));
             Console.ReadLine(); // Hold the screen
         }
 
         public static int NumDecodings(string s)
         {
             int result = 0;
+
+            Dictionary<string, string> letterToNumberMap = new Dictionary<string, string>();
+
+            for (int i = 0; i < 26; i++)
+            {
+                string letter = ((char)('A' + i)).ToString();
+                string number = (i + 1).ToString();
+                letterToNumberMap[letter] = number;
+            }
+
+            // Display the dictionary
+            foreach (var item in letterToNumberMap)
+            {
+                //Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+            //bool isContainsZero = false;
+            if (s.Contains('0'))
+            {
+                //isContainsZero = true;
+                result = s.Where(x => x != '0').Count();
+                result = 0;
+            }
+            else
+            {
+                result = s.Count();
+            }
+            if (result > 0)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char firstChar = s[i];
+                    char secondChar = new char();
+
+                    if (i > s.Length - 1)
+                    {
+                        secondChar = s[i + 1];
+                        string str = firstChar.ToString() + secondChar.ToString();
+
+                        if (letterToNumberMap.ContainsKey(str)) result++;
+                    }                    
+                }
+            }
 
             return result;
         }
