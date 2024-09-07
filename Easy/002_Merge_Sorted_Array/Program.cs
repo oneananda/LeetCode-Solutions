54c494fb6d87c9b277023270b904ba1e84ec952c
oneananda <anandathirumurugan@gmail.com>
2024-09-07 12:35:14 +0530

    Initial draft

--- Easy/002_Merge_Sorted_Array/Program.cs
+++ Easy/002_Merge_Sorted_Array/Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             // https://leetcode.com/problems/merge-sorted-array/description
-            Console.WriteLine("Hello, World!");
+
+            int[] num1 = [1, 2, 3, 0, 0, 0];
+            int[] num2 = [2, 5, 6];
+
+            Merge(num1, 3, num2, 3);
+
+            Console.ReadLine(); // Hold the screen
+        }
+
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+
         }
     }
 }
