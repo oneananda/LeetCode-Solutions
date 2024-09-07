af873d17f36eee5d6d66db1bf6c8b6ddb3b9a1e3
oneananda <anandathirumurugan@gmail.com>
2024-09-07 10:46:29 +0530

    Adding project for Merge_Sorted_Array

---
+++ Easy/002_Merge_Sorted_Array/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _002_Merge_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://leetcode.com/problems/merge-sorted-array/description
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
