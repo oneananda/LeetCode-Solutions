9f922849fd474b18fe65d5e1fd773bfbc5c4cf13
oneananda <anandathirumurugan@gmail.com>
2024-10-13 22:42:27 +0530

    Solution for _1768_Merge_Strings_Alternately

--- Easy/1768_Merge-Strings-Alternately/Program.cs
+++ Easy/1768_Merge-Strings-Alternately/Program.cs
@@ -6,9 +6,38 @@
         {
             Console.WriteLine("Enter, word 1!");
 
+            string? word1 = Console.ReadLine();
+
             Console.WriteLine("Enter, word 2!");
 
+            string? word2 = Console.ReadLine();
+
+            Console.WriteLine($"The result is: "+MergeAlternately(word1, word2)); 
+
             Console.ReadLine(); // Hold the screen
+        }
+        public static string MergeAlternately(string word1, string word2)
+        {
+            string result = string.Empty;
+            int i = 0, j = 0;
+
+            while (i < word1.Length && j < word2.Length)
+            {
+                result += word1[i++];
+                result += word2[j++];
+            }
+
+            if (i < word1.Length)
+            {
+                result += word1.Substring(i);
+            }
+
+            if (j < word2.Length)
+            {
+                result += word2.Substring(j);
+            }
+
+            return result;
         }
     }
 }
