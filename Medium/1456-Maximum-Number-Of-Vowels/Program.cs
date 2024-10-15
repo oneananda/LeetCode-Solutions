25cf2a8dc7cdccef24c543ec0e951456a6f526e4
oneananda <anandathirumurugan@gmail.com>
2024-10-15 15:22:27 +0530

    _1456_Maximum_Number_Of_Vowels

---
+++ Medium/1456-Maximum-Number-Of-Vowels/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1456_Maximum_Number_Of_Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
