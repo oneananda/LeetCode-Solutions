b1c0c2553255d15e8e7dac4d5d0b96b32dcbc48b
oneananda <anandathirumurugan@gmail.com>
2024-10-15 16:31:42 +0530

    MaxVowels solution

--- Medium/1456-Maximum-Number-Of-Vowels/Program.cs
+++ Medium/1456-Maximum-Number-Of-Vowels/Program.cs
@@ -4,15 +4,39 @@
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine(MaxVowels("abciiidef",3));
+            //Console.WriteLine(MaxVowels("abciiidef", 3));
             Console.WriteLine(MaxVowels("aeiou", 2));
-            Console.WriteLine(MaxVowels("leetcode", 3));
+            //Console.WriteLine(MaxVowels("leetcode", 3));
 
+
+            Console.ReadLine(); // Hold the screen
         }
         public static int MaxVowels(string s, int k)
         {
-            return 0;
+            int result = 0;
+
+            List<string> vowelsList = new List<string>() { "a", "e", "i", "o", "u" };
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                int count = 0;
+                var str = string.Join("", s.Skip(i).Take(k).ToList());
+
+                if(str.Length >= k)
+                {                    
+                    foreach (var item in str.ToLower())
+                    {
+                        if (vowelsList.Contains(item.ToString()))
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count > result)                
+                    result =count;        
+
+            }
+            return result;
         }
     }
 }
